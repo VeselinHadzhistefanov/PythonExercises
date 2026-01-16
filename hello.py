d08940a8426e4bc7ac219e9c280b3c8bc2ea0839
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-01-16 08:08:13 +0200

    Create python Environment and set upp all required files and inital functions

---
+++ hello.py
@@ -0,0 +1,10 @@
+loveMsg = []
+loveMsg.append("I love Sofi")
+loveMsg[0] += " <3"
+loveMsg.append("I want her to be my wife forvever, I will give her everything!")
+loveMsg.append("I will work hard every day to make her happy.")
+
+print(loveMsg)
+
+for msg in loveMsg:
+    print(msg)
